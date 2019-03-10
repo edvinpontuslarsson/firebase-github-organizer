33baffbc89b0a0cdd432ff3f64e5f771abbaebf0
Edvin Larsson <edvinpontuslarsson@gmail.com>
2019-03-10 13:36:09 +0100

    Adds youtube demo code for service worker

--- dist/firebase-messaging-sw.js
+++ dist/firebase-messaging-sw.js
@@ -1,1 +1,28 @@
-// Hello, I'm just a friendly service worker :)
+// this is a service worker file
+// uses code from this demo: https://www.youtube.com/watch?v=BsCBCudx58g
+
+importScripts('https://www.gstatic.com/firebasejs/5.8.6/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/5.8.6/firebase-messaging.js')
+
+const config = {
+    apiKey: 'AIzaSyBDrSwWXn2AklmSJtlGTJ37jpUXorwyv1c',
+    authDomain: 'gitedvinhub.firebaseapp.com',
+    databaseURL: 'https://gitedvinhub.firebaseio.com',
+    projectId: 'gitedvinhub',
+    storageBucket: 'gitedvinhub.appspot.com',
+    messagingSenderId: '821443809987'
+}
+firebase.initializeApp(config)
+
+// initializes Firebase Cloud messaging
+const messaging = firebase.messaging()
+
+messaging.setBackgroundMessageHandler(payload => {
+    const title = 'GitHub update'
+    const options = { body: payload }
+
+    return self.registration.showNotification(
+        title, options
+    )
+})
+
--- src/model/messaging.js
+++ src/model/messaging.js
@@ -7,14 +7,27 @@ const initialize = () => {
     const messaging = firebase.messaging()
     // messaging.usePublicVapidKey('AAAAv0Hd0sM:APA91bFXXwS8x3tSPhENNlVZSsuFionzjYtvqihGTznwBaOMk6HE8LRwpiYGsBmVywDUIBUsadRyXha-eFPXBXuHJXA3tWUUf0_IDX9g063tv5-m08Br44wVNJs_Lfq6WqW3D-YW8tXo')
 
-    // following this documentation: https://firebase.google.com/docs/cloud-messaging/js/client
+    // following this demo: https://www.youtube.com/watch?v=BsCBCudx58g
     messaging.requestPermission()
         .then(() => {
             console.log('User allows notifications')
+            return messaging.getToken()
+        })
+        .then(token => {
+            // TODO: store token in cloud,
+            // to be able to send messages to it
+
+            // use token for fcm API
+
+            console.log(token)
         })
         .catch(err => {
             console.log(err + 'No notifications for you!')
         })
+
+    messaging.onMessage(payload => {
+      console.log('Message received: ' + payload)  
+    })
 }
 
 export default { initialize }
