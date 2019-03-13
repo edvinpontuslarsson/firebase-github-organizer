f96f81c0707e02b7af30ae86fdc165b0a25ed711
Edvin Larsson <edvinpontuslarsson@gmail.com>
2019-03-13 12:12:29 +0100

    Something went wrong here

--- src/model/storage.js
+++ src/model/storage.js
@@ -12,8 +12,7 @@ const storeToken = token => {
 
 const storeSubscription = async (orgName, eventType) => {
   const username = auth.getUsername()
-  const token = 
-      await db().ref(`tokens/${username}`).once('value')
+  const token = db().ref(`tokens/${username}`).once('value') // await getToken()
 
   db().ref(
     `organizations/${orgName}/subscriptions/${eventType}/${username}`
@@ -23,6 +22,12 @@ const storeSubscription = async (orgName, eventType) => {
 const isSubscribed = (orgName, eventType) =>
   new Promise(async resolve => {
     const username = auth.getUsername()
+    const token = await getToken()
+
+    // https://stackoverflow.com/questions/37910008/check-if-value-exists-in-firebase-db
+
+
+
     const exists = await db().ref(
       `organizations/${orgName}/subscriptions/${eventType}/${username}`
     ).once('value')
@@ -30,6 +35,12 @@ const isSubscribed = (orgName, eventType) =>
   })
 
 const db = () => firebase.database()
+
+/**
+ * @returns {Promise<String>}
+ */
+const getToken = () => 
+  db().ref(`tokens/${username}`).once('value')
 
 // TODO: continue here: https://firebase.google.com/docs/database/web/read-and-write
 // I have now implemented "Basic write operations"
